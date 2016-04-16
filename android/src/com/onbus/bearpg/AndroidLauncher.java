eb47d3809692e92cde914dba6cc4c60d2255f0e0
Lucho <lrdolucho@gmail.com>
2016-04-15 21:30:42 -0400

    First commit

---
+++ android/src/com/onbus/bearpg/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.onbus.bearpg;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.onbus.bearpg.BeaRPG;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BeaRPG(), config);
+	}
+}
---
+++ desktop/src/com/onbus/bearpg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.onbus.bearpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.onbus.bearpg.BeaRPG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BeaRPG(), config);
+	}
+}
